376d8469d3e8b322dfaecb29a36eadef1c48beca
Fr1z <frahck@ymail.com>
2026-02-14 23:51:51 +0100

    Order By Expire Date Fix

--- js/myscript.js
+++ js/myscript.js
@@ -929,41 +929,34 @@ $(document).on("keydown", function (e) {
     }
 });
 
-//Order items for Expire Date
-$(function () {
-    function parseDate(dateStr) {
-        if (!dateStr) return null;
+// parseDate from string value
+function parseDate(dateStr) {
+    if (!dateStr) return null;
+    var parts = dateStr.trim().split('/');
+    if (parts.length !== 3) return null;
+    // new Date(anno, mese-1, giorno)
+    return new Date(parts[2], parts[1] - 1, parts[0]);
+}
 
-        var parts = dateStr.trim().split('/');
-        if (parts.length !== 3) return null;
+// Order items for Expire Date
+function orderByExpDate() {
+    var items = $('.myitem').get();
+    items.sort(function(a, b) {
+        var dateA = parseDate($(a).find('.exp-date').val());
+        var dateB = parseDate($(b).find('.exp-date').val());
+        // Se entrambe mancanti
+        if (!dateA && !dateB) return 0;
+        // Se A manca → va dopo
+        if (!dateA) return 1;
+        // Se B manca → va dopo
+        if (!dateB) return -1;
+        return dateA - dateB; // crescente
+    });
+    var $container = $('.myitems');
+    $container.append(items);
+    return;
+}
 
-        // new Date(anno, mese-1, giorno)
-        return new Date(parts[2], parts[1] - 1, parts[0]);
-    }
-
-    function orderByExpDate() {
-        var items = $('.myitem').get();
-        items.sort(function(a, b) {
-
-            var dateA = parseDate($(a).find('.exp-date').val());
-            var dateB = parseDate($(b).find('.exp-date').val());
-
-            // Se entrambe mancanti
-            if (!dateA && !dateB) return 0;
-
-            // Se A manca → va dopo
-            if (!dateA) return 1;
-
-            // Se B manca → va dopo
-            if (!dateB) return -1;
-
-            return dateA - dateB; // crescente
-        });
-        var $container = $('.myitems');
-        $container.append(items);
-        return;
-    }
-});
 
 //Order items 
 $(function () {
